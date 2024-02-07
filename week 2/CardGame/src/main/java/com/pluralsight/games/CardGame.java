8d35bfcc150629367f2cc6a57249a173e0cb6af2
NPRojas <63315208+NPRojas@users.noreply.github.com>
2024-02-07 14:32:05 -0500

    change small typo

--- week 2/CardGame/src/main/java/com/pluralsight/games/CardGame.java
+++ week 2/CardGame/src/main/java/com/pluralsight/games/CardGame.java
@@ -21,22 +21,22 @@ public class CardGame
         // keep track of a winning hand
         Hand winningPlayer = players.get(0);
         // loop through the players and deal the cards from the deck
-        for (Hand player: players) {
-
-            // deal a card twice for each player (as per rules of Black Jack)
-            for (int i = 0; i < 2; i++) {
-                // remove a card from the deck
-                Card cardDealt = deck.deal();
-                // add it to the players Hand
-                player.deal(cardDealt);
-            }
-
-            if (player.getValue() > winningPlayer.getValue()) {
-                winningPlayer = player;
-            }
-        }
+//        for (Hand player: players) {
+//
+//            // deal a card twice for each player (as per rules of Black Jack)
+//            for (int i = 0; i < 2; i++) {
+//                // remove a card from the deck
+//                Card cardDealt = deck.deal();
+//                // add it to the players Hand
+//                player.deal(cardDealt);
+//            }
+//
+//            if (player.getValue() > winningPlayer.getValue()) {
+//                winningPlayer = player;
+//            }
+//        }
 
         // display the winner
-        System.out.println("The winning player is " + winningPlayer.getPlayerName() + "with a value of " + winningPlayer.getValue());
+        System.out.println("The winning player is " + winningPlayer.getPlayerName() + "with a hand of " + winningPlayer.getValue());
     }
 }
