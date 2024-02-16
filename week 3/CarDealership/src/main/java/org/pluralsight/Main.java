10e440494e00058ae8cd86c4613d25de1cef8ed9
NPRojas <63315208+NPRojas@users.noreply.github.com>
2024-02-16 11:49:37 -0500

    got search by year search working in main

--- week 3/CarDealership/src/main/java/org/pluralsight/Main.java
+++ week 3/CarDealership/src/main/java/org/pluralsight/Main.java
@@ -1,7 +1,19 @@
 package org.pluralsight;
 
+import org.pluralsight.models.Vehicle;
+import org.pluralsight.services.DealershipFileManager;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DealershipFileManager dealershipFileManager = new DealershipFileManager();
+
+        ArrayList<Vehicle> vehicles = dealershipFileManager.getVehiclesByYear(2012);
+
+        // Print the vehicles
+        for (Vehicle vehicle : vehicles) {
+            System.out.println(vehicle.getMake());
+        }
     }
 }
